575b208112576a84d36157103f3b75dcded5ee66
zertmark <47692978+zertmark@users.noreply.github.com>
2020-03-27 23:54:37 +0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-pip3 install colorama progressbar scapy argparse netifaces
+pip3 install colorama progressbar scapy argparse netifaces                                              
+chmod +x sniffer && chmod +x sniffer_black_theme                     
